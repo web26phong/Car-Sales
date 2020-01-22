bb58f7298a7d6d36f21cd1e98dea3cbec4d68ea4
Phong Yang <phongyang88@hotmail.com>
2020-01-22 15:44:21 -0600

    setup empty reducer and initial state. setup store and provider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+import {carReducer} from "./reducers/carReducer";
+
+export const store = createStore(carReducer);
+
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
