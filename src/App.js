8c3883c422c4aa9955a5ce2e1c9d460c5947b800
Phong Yang <phongyang88@hotmail.com>
2020-01-22 16:27:58 -0600

    connected components. mapped state to props in app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import Total from './components/Total';
 import {carReducer} from "./reducers/carReducer";
 
 export const store = createStore(carReducer);
+console.log("This is the initial state in store: ",store.getState())
 
-const App = () => {
+const App = (props) => {
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
@@ -24,15 +25,23 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  };
+};
+
+export default connect(mapStateToProps, {})(App);
